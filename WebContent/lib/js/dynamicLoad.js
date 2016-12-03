2ff982462a05242a3a1b042adbe3da365e32ed7a
Zaman Asrar <asrarzaman.m@gmail.com>
2016-12-03 11:16:37 +0800

    Javascript base - anyconversion

---
+++ WebContent/lib/js/dynamicLoad.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	var hash = window.location.hash.substr(1);
+	var prevHash='any';
+	/**
+	 * THE FOLLOWING BLOCK CHECKS IF THE INITAL URL CONTAINS HASH STRING
+	 * AND IF IT FINDS A VALID HASH STRING IT REDIRECTS TO THAT PARTICULAR CONVERSION PAGE
+	 */
+	if(hash){
+		var href = hash+".html";
+		var hrefElement = $('a[href="'+href+'"]');
+		if(hrefElement){
+			loadPage(href);
+			prevHash=hash;
+			
+		}
+	}
+
+	
+   $('.mdl-navigation__link').click(function(){	     
+	    var href = $(this).attr('href');
+	    loadPage(href);
+	    return false;  
+	    });
+
+   /**
+    * FUNCTION TO DYNAMICALLY LOAD THE GIVEN HTML PAGE AND REPLACE 'details' DIV
+    */
+   function loadPage(href){	     
+	    var toLoad = href+' #content';
+	    var hash = href.substr(0,href.length-5);
+	    if(hash === prevHash){
+	    	return;
+	    }
+	    prevHash =hash;
+	    $('#details').hide('fast',loadContent);
+	    window.location.hash = hash;	    
+	    function loadContent() {
+	        $('#details').load(toLoad,'',showNewContent());
+	        $(document).trigger("categoryChange",[hash]);
+	    }
+	    function showNewContent() {
+	        $('#details').show('normal');
+	    } 
+   }
+});
